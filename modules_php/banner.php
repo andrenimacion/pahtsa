20bfc095ef20cedaef42566c44bf8db4f6982ef1
akali <andrenimacion@gmail.com>
2019-09-15 13:37:37 -0500

    cambio a banners

--- modules_php/banner.php
+++ modules_php/banner.php
@@ -1,13 +1,40 @@
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <img src="./img/tienda1.jpg" class="d-block w-100" alt="...">
+<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./img/tienda1" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>¡Abre tu tienda con nosotros!</h5>
+          <p>Tu tienda y productos siempre disponibles.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./img/compradoras.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Tus productos</h5>
+          <p>Todos los productos a nivel nacional al alcance de tu mano.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="comercio.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Comercio seguro</h5>
+          <p>Tus compras siempre estarán seguras.</p>
+        </div>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="./img/tienda2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/compradoras.jpg" class="d-block w-100" alt="...">
-    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </div>
